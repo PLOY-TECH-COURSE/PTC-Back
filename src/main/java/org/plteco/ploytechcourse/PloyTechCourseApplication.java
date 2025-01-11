0d9cac5bdee2d5048ff9b6af639aaaebd673f1d3
jaeminjo732 <jaeminjo732@gmail.com>
2025-01-11 12:46:39 +0900

    feat :: DDD 형식으로 폴더 구성

---
+++ src/main/java/org/plteco/ploytechcourse/PloyTechCourseApplication.java
@@ -0,0 +1,13 @@
+package org.plteco.ploytechcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloyTechCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PloyTechCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/plteco/ploytechcourse/PloyTechCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.plteco.ploytechcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloyTechCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
