adf1359c948f6f40adde2a051caca6de383bdffb
heodongun <heodongun0922@gmail.com>
2025-01-16 11:32:05 +0900

    feat(#5) :: 이메일 인증

--- src/main/java/org/plteco/ploytechcourse/shared/config/SecurityConfig.java
+++ src/main/java/org/plteco/ploytechcourse/shared/config/SecurityConfig.java
@@ -3,16 +3,24 @@ package org.plteco.ploytechcourse.shared.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 //
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests()
-                .anyRequest().permitAll();
+        http.authorizeHttpRequests((auth)->auth
+                .anyRequest().permitAll()
+        );
+        http.csrf((auth)->auth.disable());
         return http.build();
     }
 }
