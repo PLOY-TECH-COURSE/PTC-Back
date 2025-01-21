c2bc50dd79fda87407823ec89dbddf17a54d0edc
heodongun <heodongun0922@gmail.com>
2025-01-21 14:36:38 +0900

    feat(#15) :: 테크코스 신청받기

---
+++ src/main/java/org/plteco/ploytechcourse/application/application/service/ApplyApplicationApplication.java
@@ -0,0 +1,7 @@
+package org.plteco.ploytechcourse.application.application.service;
+
+import org.plteco.ploytechcourse.application.application.dto.ApplyApplicationDto;
+
+public interface ApplyApplicationApplication {
+    String applyApplication(ApplyApplicationDto applyApplicationDto);
+}
---
+++ src/main/java/org/plteco/ploytechcourse/domain/application/service/ApplyApplicationServiceImpl.java
@@ -0,0 +1,31 @@
+package org.plteco.ploytechcourse.domain.application.service;
+
+import lombok.RequiredArgsConstructor;
+import org.plteco.ploytechcourse.application.application.dto.ApplyApplicationDto;
+import org.plteco.ploytechcourse.domain.application.model.TechCourseForm;
+import org.plteco.ploytechcourse.domain.application.repository.ApplicationRepository;
+import org.plteco.ploytechcourse.domain.user.signup.model.entity.User;
+import org.plteco.ploytechcourse.shared.jwt.UserContextUtil;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class ApplyApplicationServiceImpl implements ApplyApplicationService {
+    private final UserContextUtil userContextUtil;
+    private final ApplicationRepository applicationRepository;
+
+    @Override
+    public TechCourseForm apply(ApplyApplicationDto applyApplicationDto) {
+        return applicationRepository.save(TechCourseForm.builder()
+                        .user(userContextUtil.getCurrentUser())
+                        .introduction(applyApplicationDto.getIntroduction())
+                        .resolution(applyApplicationDto.getResolution()).build());
+    }
+
+    @Override
+    public boolean isValidDuplicationStudent(Long userId) {
+        return applicationRepository.existsByUserId(userId);
+    }
+}
