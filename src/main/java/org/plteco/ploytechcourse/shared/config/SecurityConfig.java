ad6c19b454f2258bc64ee9b08ab4a67499fbc524
heodongun <heodongun0922@gmail.com>
2025-01-23 16:28:33 +0900

    fix(#35) :: Jwt와 로그인 필터의 예외가 처리가 안되어있어서 했습니다.

--- src/main/java/org/plteco/ploytechcourse/shared/config/SecurityConfig.java
+++ src/main/java/org/plteco/ploytechcourse/shared/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
 
                         //클라이언트 주소 넣어야함
 //                       configuration.setAllowedOrigins(Collections.singletonList("https://ptc-front-bves.vercel.app"));
-                        configuration.setAllowedOrigins(Arrays.asList("https://ptc-front-bves.vercel.app", "http://192.168.0.18:5173","http://192.168.0.17:5173"));
+                        configuration.setAllowedOrigins(Arrays.asList("https://ptc-front-bves.vercel.app", "http://192.168.0.18:5173","http://192.168.0.17:5173","http://192.168.0.27:5173","http://172.21.62.81:5173"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
