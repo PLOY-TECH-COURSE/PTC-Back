a166233a65e4cdf8a98241763154b025621d5866
jaeminjo732 <jaeminjo732@gmail.com>
2025-01-28 21:32:28 +0900

    feat(#26) :: commentlike application, service test 작성

--- src/main/java/org/plteco/ploytechcourse/application/like/CommentLikeServiceApplication.java
+++ src/main/java/org/plteco/ploytechcourse/application/like/CommentLikeServiceApplication.java
@@ -1,32 +1,30 @@
-package org.plteco.ploytechcourse.application.like;
-
-import lombok.RequiredArgsConstructor;
-import org.plteco.ploytechcourse.domain.like.commentlike.repository.CommentLikeRepository;
-import org.plteco.ploytechcourse.domain.like.commentlike.service.CommentLikeService;
-import org.plteco.ploytechcourse.domain.user.signup.repository.UserRepository;
-import org.plteco.ploytechcourse.shared.jwt.UserContextUtil;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class CommentLikeServiceApplication {
-
-    private final CommentRepository commentRepository;
-    private final UserRepository userRepository;
-    private final CommentLikeRepository commentLikeRepository;
-
-    public void addLike(Long commentId, Long userId) {
-        if (userRepository.existsById(userId)) {
-
-        }
-        if (commentRepository.existsById(commentId)){
-
-        }
-        if (commentLikeRepository.existsByCommentIdAndUserId(commentId, userId)) {
-
-        }
-    }
-
-
-
-}
+//package org.plteco.ploytechcourse.application.like;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.plteco.ploytechcourse.domain.like.commentlike.repository.CommentLikeRepository;
+//import org.plteco.ploytechcourse.domain.user.signup.repository.UserRepository;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class CommentLikeServiceApplication {
+//
+//    private final CommentRepository commentRepository;
+//    private final UserRepository userRepository;
+//    private final CommentLikeRepository commentLikeRepository;
+//
+//    public void addLike(Long commentId, Long userId) {
+//        if (userRepository.existsById(userId)) {
+//
+//        }
+//        if (commentRepository.existsById(commentId)){
+//
+//        }
+//        if (commentLikeRepository.existsByCommentIdAndUserId(commentId, userId)) {
+//
+//        }
+//    }
+//
+//
+//
+//}
