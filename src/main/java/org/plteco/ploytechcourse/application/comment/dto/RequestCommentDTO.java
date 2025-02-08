062d541b4ee7657f9d23dd698a93ac713d67e858
jaeminjo732 <jaeminjo732@gmail.com>
2025-02-09 00:27:18 +0900

    feat(#16) :: swagger 조금 수정

--- src/main/java/org/plteco/ploytechcourse/application/comment/dto/RequestCommentDTO.java
+++ src/main/java/org/plteco/ploytechcourse/application/comment/dto/RequestCommentDTO.java
@@ -1,11 +1,13 @@
 package org.plteco.ploytechcourse.application.comment.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class RequestCommentDTO {
+    @Schema(description = "댓글 데이터", example = "어느 옛날옛적에 대머리인 허동운과 허온이 살았더래요")
     @NotBlank(message = "댓글 내용이 작성되지 않았습니다.")
     @Size(max = 500, message = "댓글은 500자를 초과할 수 없습니다.")
     String commentText;
