16e850fe3b527ce5d8de412a0cecfa2418efdc67
huhonmcmc <huhonmcmc@gmail.com>
2025-02-17 18:49:45 +0900

    feat/#64 :: 글 작성, 보기, 자세히보기 api 구현 완료

--- src/main/java/org/plteco/ploytechcourse/application/document/dto/DocumentInfoDTO.java
+++ src/main/java/org/plteco/ploytechcourse/application/document/dto/DocumentInfoDTO.java
@@ -1,19 +1,19 @@
 package org.plteco.ploytechcourse.application.document.dto;
 
-import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
-public record DocumentInfoDTO (
-    @NotNull(message = "글 정보에 id(글 id)가 비어있습니다.")
-    Long id,
-
-    @NotNull(message = "글 정보에 title(글 제목)가 비어있습니다.")
-    String title,
-
-    @NotNull(message = "글 정보에 introduction(글 소개)가 비어있습니다.")
-    String contents,
-
-    @NotNull(message = "글 정보에 date(글 작성 날짜)가 비어있습니다.")
-    LocalDate date
-) {}
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class DocumentInfoDTO {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDate createAt;
+}
--- src/main/java/org/plteco/ploytechcourse/application/document/dto/DocumentUserInfoDTO.java
+++ src/main/java/org/plteco/ploytechcourse/application/document/dto/DocumentUserInfoDTO.java
@@ -1,14 +1,16 @@
 package org.plteco.ploytechcourse.application.document.dto;
 
-import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public record DocumentUserInfoDTO(
-        @NotNull(message = "작성자 정보에 id(작성자 id)가 비어있습니다.")
-        Long id,
-
-        @NotNull(message = "작성자 정보에 name(작성자 이름)이 비어있습니다.")
-        String name,
-
-        @NotNull(message = "작성자 정보에 profile(작성자 프로필)이 비어있습니다.")
-        String profile
-) {}
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class DocumentUserInfoDTO {
+    private Long id;
+    private String uid;
+    private String profile;
+}
