000cbb86b6ea9eccea86b405da864b0d56af86ca
jaeminjo732 <jaeminjo732@gmail.com>
2025-02-17 16:44:13 +0900

    feat(#9) :: commentDTO 수정

--- src/main/java/org/plteco/ploytechcourse/application/comment/dto/CommentDTO.java
+++ src/main/java/org/plteco/ploytechcourse/application/comment/dto/CommentDTO.java
@@ -1,21 +1,18 @@
 package org.plteco.ploytechcourse.application.comment.dto;
 
 import jakarta.validation.Valid;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import org.plteco.ploytechcourse.domain.comment.model.entity.Comment;
-
+@Setter
 @Getter
 public class CommentDTO {
     private long id;
-    private long userId;
     private String comment;
+    private long userId;
     private String userName;
     private String userProfile;
-    public CommentDTO(Comment comment) {
-        this.id = comment.getId();
-        this.userId = comment.getUser().getId();
-        this.comment = comment.getComment();
-        this.userName = comment.getUser().getName();
-        this.userProfile = comment.getUser().getProfile();
-    }
+    private long likeCount;
+    private boolean Liked;
 }
