95c66009181a89f665b462e3a5da79cd5bc8bf10
jaeminjo732 <jaeminjo732@gmail.com>
2025-02-25 23:19:22 +0900

    feat(#92) :: Cors제거

--- src/main/java/org/plteco/ploytechcourse/shared/config/CorsMvcConfig.java
+++ src/main/java/org/plteco/ploytechcourse/shared/config/CorsMvcConfig.java
@@ -24,10 +24,6 @@ public class CorsMvcConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowedOriginPatterns("https://ptc-front-bves.vercel.app") // 모든 출처 허용
-                .allowedOriginPatterns("http://192.168.10.51:5173")
-                .allowedOriginPatterns("http://192.168.45.90:5173")
-                .allowedOriginPatterns("http://172.30.1.15:5173")
-                .allowedOriginPatterns("http://192.168.219.110:5173")
                 .allowCredentials(true); // 자격 증명 허용
     }
 }
