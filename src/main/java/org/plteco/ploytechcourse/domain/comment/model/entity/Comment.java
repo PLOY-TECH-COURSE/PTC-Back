2c7dde5ebf748637ec37fb758ec4c987cf6b08e5
jaeminjo732 <jaeminjo732@gmail.com>
2025-02-27 01:16:16 +0900

    fix(#103) :: 시큐리티 추가

--- src/main/java/org/plteco/ploytechcourse/domain/comment/model/entity/Comment.java
+++ src/main/java/org/plteco/ploytechcourse/domain/comment/model/entity/Comment.java
@@ -27,6 +27,7 @@ public class Comment {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @Setter
     @Column(name = "comment", nullable = false, length = 500)
     private String comment;
 
